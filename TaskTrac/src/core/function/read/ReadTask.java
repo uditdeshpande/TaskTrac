03ada87ba0a6060e40d438c8ed3e07c2dc080c53
uditdeshpande <deshpande.udit@gmail.com>
2014-01-04 17:52:44 +0530

    serialized the task class and added all tasks to the temp.txt file!

--- TaskTrac/src/core/function/read/ReadTask.java
+++ TaskTrac/src/core/function/read/ReadTask.java
@@ -1,13 +1,21 @@
 package core.function.read;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import core.global.env.Task;
+
 
 public class ReadTask implements Runnable{
 
 	private Process process_read;
+	
+	Task temp;
 	
 	/* Reads the tasklist from the command prompt command. Then this scanner class will differentiate the various parameters*/
 	public void run() {
@@ -15,8 +23,42 @@ public class ReadTask implements Runnable{
 		process_read=Runtime.getRuntime().exec("tasklist");
 		Scanner task_scanner=new Scanner(new InputStreamReader(process_read.getInputStream()));
 	//first 4 lines are headers null lines and the system idle process variable.
-		task_scanner.next();task_scanner.next();task_scanner.next();task_scanner.next();
+		task_scanner.nextLine();task_scanner.nextLine();task_scanner.nextLine();task_scanner.nextLine();
 		
+		while(task_scanner.hasNextLine())
+		{
+			/*
+			System.out.println("1"+task_scanner.next());
+			System.out.println("2"+task_scanner.nextInt());
+			System.out.println("3"+task_scanner.next());
+			System.out.println("4"+task_scanner.nextInt());
+			System.out.println("5"+task_scanner.nextDouble());
+			task_scanner.nextLine();
+			*/
+			temp=new Task();
+			try{
+			if(task_scanner.hasNext())
+			temp.setImage_name(task_scanner.next());
+			if(task_scanner.hasNextInt())
+			temp.setPid(task_scanner.nextInt());
+			if(task_scanner.hasNext())
+			temp.setSession_name(task_scanner.next());
+			if(task_scanner.hasNextInt())
+			temp.setSession_no(task_scanner.nextInt());
+			if(task_scanner.hasNextDouble())
+			temp.setMem_usage(task_scanner.nextDouble());
+			task_scanner.nextLine();
+			System.out.println(temp.toString());
+			//add to array list
+			//tlist.task_list.add(temp);
+			FileOutputStream ser_file=new FileOutputStream("C:\\Users\\User\\git\\TaskTrac\\TaskTrac\\src\\core\\function\\read\\temp.txt");
+			ObjectOutputStream oout=new ObjectOutputStream(ser_file);
+			oout.writeObject(temp);
+			}
+			catch (Exception e) {
+			System.out.println("Error");
+			}
+			}
 		
 		
 		} catch (IOException e) {
@@ -30,6 +72,7 @@ public class ReadTask implements Runnable{
 	{
 		Thread t=new Thread(new ReadTask(),"mma");
 		t.start();
+	
 	}
 
 }
--- TaskTrac/src/core/global/env/Task.java
+++ TaskTrac/src/core/global/env/Task.java
@@ -1,12 +1,13 @@
 package core.global.env;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Task{
-	String Image_name;
-	int pid;
-	String Session_name;
-	int Session_no;
-	double mem_usage;
+public class Task implements Serializable{
+	private String Image_name;
+	private int pid;
+	private String Session_name;
+	private int Session_no;
+	private double mem_usage;
 	
 	public Task() {
 		Image_name="";
@@ -14,6 +15,63 @@ public class Task{
 		Session_name="";
 		Session_no=-1;
 		mem_usage=0; 
+	}
+
+	public Task(String image_name, int pid, String session_name,
+			int session_no, double mem_usage) {
+		super();
+		Image_name = image_name;
+		this.pid = pid;
+		Session_name = session_name;
+		Session_no = session_no;
+		this.mem_usage = mem_usage;
+	}
+
+	public String getImage_name() {
+		return Image_name;
+	}
+
+	public void setImage_name(String image_name) {
+		Image_name = image_name;
+	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getSession_name() {
+		return Session_name;
+	}
+
+	public void setSession_name(String session_name) {
+		Session_name = session_name;
+	}
+
+	public int getSession_no() {
+		return Session_no;
+	}
+
+	public void setSession_no(int session_no) {
+		Session_no = session_no;
+	}
+
+	public double getMem_usage() {
+		return mem_usage;
+	}
+
+	public void setMem_usage(double mem_usage) {
+		this.mem_usage = mem_usage;
+	}
+
+	public String toString() {
+		return "Task [Image_name=" + Image_name + ", pid=" + pid
+				+ ", Session_name=" + Session_name + ", Session_no="
+				+ Session_no + ", mem_usage=" + mem_usage + "]";
 	}	
+	
 	
 }
