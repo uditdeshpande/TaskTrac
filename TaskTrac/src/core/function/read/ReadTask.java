59e9d6e28f989c51adc20956a64b03beaada2b42
uditdeshpande <deshpande.udit@gmail.com>
2014-01-04 20:09:54 +0530

    task display deserializes the Task object!

--- TaskTrac/src/core/function/read/ReadTask.java
+++ TaskTrac/src/core/function/read/ReadTask.java
@@ -16,17 +16,23 @@ public class ReadTask implements Runnable{
 	private Process process_read;
 	
 	Task temp;
-	
+	FileOutputStream ser_file;
+	ObjectOutputStream oout;
 	/* Reads the tasklist from the command prompt command. Then this scanner class will differentiate the various parameters*/
 	public void run() {
 	try {
+		ser_file=new FileOutputStream("C:\\Users\\User\\git\\TaskTrac\\TaskTrac\\src\\core\\function\\read\\temp.txt");
+		oout=new ObjectOutputStream(ser_file);
 		process_read=Runtime.getRuntime().exec("tasklist");
 		Scanner task_scanner=new Scanner(new InputStreamReader(process_read.getInputStream()));
 	//first 4 lines are headers null lines and the system idle process variable.
 		task_scanner.nextLine();task_scanner.nextLine();task_scanner.nextLine();task_scanner.nextLine();
 		
+		
 		while(task_scanner.hasNextLine())
 		{
+			
+			
 			/*
 			System.out.println("1"+task_scanner.next());
 			System.out.println("2"+task_scanner.nextInt());
@@ -51,13 +57,14 @@ public class ReadTask implements Runnable{
 			System.out.println(temp.toString());
 			//add to array list
 			//tlist.task_list.add(temp);
-			FileOutputStream ser_file=new FileOutputStream("C:\\Users\\User\\git\\TaskTrac\\TaskTrac\\src\\core\\function\\read\\temp.txt");
-			ObjectOutputStream oout=new ObjectOutputStream(ser_file);
 			oout.writeObject(temp);
+			
 			}
 			catch (Exception e) {
 			System.out.println("Error");
 			}
+		
+			
 			}
 		
 		
@@ -65,14 +72,21 @@ public class ReadTask implements Runnable{
 		
 		e.printStackTrace();
 		}
-	
+	try {
+		oout.close();
+		ser_file.close();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	}
 
 	public static void main(String []args)
 	{
 		Thread t=new Thread(new ReadTask(),"mma");
 		t.start();
-	
+		
 	}
+	
 
 }
